f7a71ed37a189d1ff51b8a54541c5ff7653b2b1f
Kevin <kevindcnlla@gmail.com>
2023-12-05 18:58:18 +0800

    Pull request for incomplete friend request updated

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
@@ -18,5 +17,5 @@ class Comment extends Model
     {
         return $this->belongsTo(Post::class);
     }
-    use HasFactory;
+    
 }
